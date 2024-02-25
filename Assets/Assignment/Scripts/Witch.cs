3aecfafa6e4d6113ce98a2d8f963d47fde2bd073
lnxnri <goodp@sheridancollege.ca>
2024-02-24 19:08:35 -0500

    Added attack animation + animation curve on run

--- Assets/Assignment/Scripts/Witch.cs
+++ Assets/Assignment/Scripts/Witch.cs
@@ -5,7 +5,7 @@ using static UnityEngine.GraphicsBuffer;
 
 public class Witch : MonoBehaviour
 {
-    public float walkSpeed = 5f;
+  
     public float horizontalMovement = 0f;
     public float inputHorizontal;
     public Vector2 movement;
@@ -14,11 +14,14 @@ public class Witch : MonoBehaviour
     Rigidbody2D rb;
     Animator animators;
     bool facingRight = true;
+    public AnimationCurve movementCurve;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animators = GetComponent<Animator>();
+
         
     }
 
@@ -27,7 +30,7 @@ public class Witch : MonoBehaviour
     {
         inputHorizontal = movement.x;
         movement = destination - (Vector2)transform.position;
-
+    //if character is not moving, stand still
         if (movement.magnitude < 0.1 && facingRight)
         {
            movement = Vector2.zero;      
@@ -36,6 +39,7 @@ public class Witch : MonoBehaviour
         {
             movement = Vector2.zero;
         }
+        //if character is moving right or left, flip to according direction
         if (inputHorizontal > 0 && !facingRight)
         {
             Flip();
@@ -44,18 +48,28 @@ public class Witch : MonoBehaviour
         {
             Flip();
         }
+        float curveValue = movementCurve.Evaluate(Time.time); // Evaluate the curve at the current time
+        movement *= curveValue;
 
     }
     private void Update()
     {
+        //move character with left click
         if (Input.GetMouseButtonDown(0))
         {
             destination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             destination.y = transform.position.y;
         }
+        //attack with right click
+        if (Input.GetMouseButtonDown(1))
+        {
+            animators.SetTrigger("attack");
+        }
         animators.SetFloat("Speed", movement.magnitude);
         rb.MovePosition(rb.position + movement.normalized * horizontalMovement * Time.deltaTime);
     }
+   
+    //function to check which way character is facing
     void Flip()
     {
         Vector3 currentScale = gameObject.transform.localScale;
