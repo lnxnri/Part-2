20c9a31941f3d9be0bf585e01b609cc2380d1fe6
lnxnri <goodp@sheridancollege.ca>
2024-02-25 22:41:33 -0500

    fixed walker, removed flips

--- Assets/Assignment/Scripts/Witch.cs
+++ Assets/Assignment/Scripts/Witch.cs
@@ -1,53 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 using static UnityEngine.GraphicsBuffer;
 
 public class Witch : MonoBehaviour
 {
   
     public float horizontalMovement = 0f;
-    public float inputHorizontal;
-    public Vector2 movement;
+    private Vector2 movement;
     private Vector2 destination;
     public Animator animator;
     Rigidbody2D rb;
     Animator animators;
-    bool facingRight = true;
     public AnimationCurve movementCurve;
+    private float yPosition = -4f;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animators = GetComponent<Animator>();
-
-        
+        destination.y = yPosition;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        inputHorizontal = movement.x;
         movement = destination - (Vector2)transform.position;
-    //if character is not moving, stand still
-        if (movement.magnitude < 0.1 && facingRight)
+  //  if character is not moving, stand still
+        if (movement.magnitude < 0.1)
         {
-           movement = Vector2.zero;      
+           movement = Vector2.zero;       
         }
-        if (movement.magnitude < 0.1 && !facingRight)
-        {
-            movement = Vector2.zero;
-        }
-        //if character is moving right or left, flip to according direction
-        if (inputHorizontal > 0 && !facingRight)
-        {
-            Flip();
-        }
-        if (inputHorizontal < 0 && facingRight) 
-        {
-            Flip();
-        }
+    
         float curveValue = movementCurve.Evaluate(Time.time); // Evaluate the curve at the current time
         movement *= curveValue;
 
@@ -59,6 +45,7 @@ public class Witch : MonoBehaviour
         {
             destination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             destination.y = transform.position.y;
+
         }
         //attack with right click
         if (Input.GetMouseButtonDown(1))
@@ -67,15 +54,8 @@ public class Witch : MonoBehaviour
         }
         animators.SetFloat("Speed", movement.magnitude);
         rb.MovePosition(rb.position + movement.normalized * horizontalMovement * Time.deltaTime);
+        
     }
    
-    //function to check which way character is facing
-    void Flip()
-    {
-        Vector3 currentScale = gameObject.transform.localScale;
-        currentScale.x *= -1;
-        gameObject.transform.localScale = currentScale;
-
-        facingRight = !facingRight;
-    }
+   
 }
